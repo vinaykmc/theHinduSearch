f2dffada56d7fef463859517bab788b5338819f7
vinaykmc <vinaykumarmc@rediff.com>
2018-12-28 10:29:55 +0530

    Rename of file

--- src/main/java/sample/hindu/search/controller/SearchController.java
+++ src/main/java/sample/hindu/search/controller/SearchController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import sample.hindu.search.FeignClients;
 
 @Controller
-public class MessageController {
+public class SearchController {
 	
 	@Autowired
 	FeignClients feignClients;
